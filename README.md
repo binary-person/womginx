413ac50e794b8931ecedd02c481b76c2b9597732
Simon Cheng <simoncheng559@gmail.com>
2022-01-14 04:11:31 +0000

    add replit support (closes #72)

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 > Fastest proxy using only nginx as the backend server
 
-**Before you deploy to Heroku, read the following: apparently, heroku decided that this repository violates the ToS, so directly clicking the button below if you're in the main repository will result in an error. You can avoid this by forking a repo, then clicking on this deploy button on your own cloned repo. However, I am NOT responsible if your heroku account gets suspended because of deploying this application. Proceed at your own risk.**
+<a href="https://repl.it/github/binary-person/womginx"><img src="https://docs.replit.com/images/repls/run-on-replit.svg" alt="Replit" width="150"/></a>
+
+**Before you deploy to Heroku, read the following: Heroku decided that this repository violates the ToS, so directly clicking the button below if you're in the main repository will result in an error. Forked repositories may work, though this is not guaranteed. Regardless, I am NOT responsible if your Heroku account gets suspended because of deploying this application. Proceed at your own risk.**
 
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
 
--- replit-run.sh
+++ replit-run.sh
@@ -1,3 +1,48 @@
 #!/bin/bash
 
+set -e
+
+DIR=$(pwd)
+ESCAPED_DIR=$(echo $DIR | sed -e 's/\//\\\//g')
+
 git pull
+
+# install and build wombat or just update if the folder already exists
+if [ ! -d $DIR/public/wombat/dist ]; then
+    git submodule update --init --recursive
+    cd $DIR/public/wombat
+    npm install && npm run build-prod
+fi
+
+cd $DIR
+
+# modify replit-nginx.conf without modifying nginx.conf to avoid git pull conflict message
+rm -rf replit-nginx-temp
+mkdir replit-nginx-temp
+cp nginx.conf replit-nginx-temp/
+cd replit-nginx-temp
+../docker-sed.sh
+
+# replace with appropriate public dir path
+sed -i "s/\/opt\/womginx\/public/$ESCAPED_DIR\/public/g" nginx.conf
+
+# add missing mime.types
+curl https://raw.githubusercontent.com/nginx/nginx/master/conf/mime.types -o mime.types
+sed -i "s/\/etc\/nginx\/mime.types/$ESCAPED_DIR\/replit-nginx-temp\/mime.types/g" nginx.conf
+
+# for some reason, the 1.1.1.1 and 8.8.8.8 dns doesn't work
+sed -i "s/1.1.1.1/1.1.1.3/" nginx.conf
+
+# point cache folders to writable directories
+rm -rf /tmp/womginx-cache
+mkdir /tmp/womginx-cache
+CACHE_DIR="\/tmp\/womginx-cache"
+sed -i $"s/http {/http {\
+client_body_temp_path $CACHE_DIR\/client_body;\
+      proxy_temp_path $CACHE_DIR\/proxy;\
+    fastcgi_temp_path $CACHE_DIR\/fastcgi;\
+      uwsgi_temp_path $CACHE_DIR\/uwsgi;\
+       scgi_temp_path $CACHE_DIR\/scgi;\
+/" nginx.conf
+
+nginx -c $DIR/replit-nginx-temp/nginx.conf -g 'error_log stderr;'
